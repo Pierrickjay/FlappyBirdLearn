92c547fb8c27e2b5f4449cd49e6b6762d81c18f5
Pierrick Jay <pierrick.jay1@gmail.com>
2023-04-02 14:18:18 +0200

    menu best score done

--- game.js
+++ game.js
@@ -1,6 +1,23 @@
+const menu = document.querySelector('.menu');
+const animation = document.querySelector('.animation');
+const jouerLink = document.querySelector('.menu li:first-child a');
+const bestScoreLink = document.querySelector('#best-score');
+const menuBestScore = document.querySelector('.menuBestScore');
 
-function flappyJumping()
-{
-	info("flappy jump");
-	document.getElementById("demo").innerHTML = "Hello JavaScript!";
-}
+jouerLink.addEventListener('click', () => {
+// On modifie la propriété "display" du menu pour le masquer
+menu.style.display = 'none';
+});
+bestScoreLink.addEventListener('click', () => {
+	// Masquer le menu principal
+	menu.style.display = 'none';
+	// Afficher le menu de meilleur score
+	menuBestScore.style.display = 'block';
+	animation.style.display ='none';
+	const backArrowLink = document.querySelector('#back');
+	backArrowLink.addEventListener('click', () => {
+		menu.style.display = 'block';
+		animation.style.display ='block';
+		menuBestScore.style.display = 'none';
+	})
+});
