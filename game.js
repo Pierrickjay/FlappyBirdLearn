bf1fba81254067e31cb05ac799b8d2f4d631dad2
Pierrick Jay <pierrick.jay1@gmail.com>
2023-04-02 15:13:19 +0200

    tubes is not moving

--- game.js
+++ game.js
@@ -5,9 +5,19 @@ const bestScoreLink = document.querySelector('#best-score');
 const menuBestScore = document.querySelector('.menuBestScore');
 
 jouerLink.addEventListener('click', () => {
-// On modifie la propriété "display" du menu pour le masquer
-menu.style.display = 'none';
-});
+	// On modifie la propriété "display" du menu pour le masquer
+	menu.style.display = 'none';
+	const game = document.querySelector('.game')
+	const tubeUp = document.querySelector('#tubeUp');
+	const tubeDown = document.querySelector('#tubeDown');
+	game.style.display = 'block';
+	setInterval(() => {
+	  tubeUp.style.left = (parseInt(tubeUp.style.left) - 5) + '%';
+	  tubeDown.style.left = (parseInt(tubeDown.style.left) - 5) + '%';
+	}, 1000);
+  });
+
+
 bestScoreLink.addEventListener('click', () => {
 	// Masquer le menu principal
 	menu.style.display = 'none';
@@ -15,6 +25,7 @@ bestScoreLink.addEventListener('click', () => {
 	menuBestScore.style.display = 'block';
 	animation.style.display ='none';
 	const backArrowLink = document.querySelector('#back');
+	// Retour en arriere
 	backArrowLink.addEventListener('click', () => {
 		menu.style.display = 'block';
 		animation.style.display ='block';
